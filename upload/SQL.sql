039b67c3bae23a8c8bb246c9d7d6116a5af08f3e
Muhammad Abubakar <63736631+abubakarirfan@users.noreply.github.com>
2022-04-17 00:28:20 +1000

    Update SQL.sql

--- upload/SQL.sql
+++ upload/SQL.sql
@@ -1,31 +1,12 @@
--- phpMyAdmin SQL Dump
--- version 5.1.0
--- https://www.phpmyadmin.net/
---
--- Host: 127.0.0.1
--- Generation Time: Apr 12, 2022 at 01:20 AM
--- Server version: 10.4.18-MariaDB
--- PHP Version: 8.0.3
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
 
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Database: `lms`
---
 CREATE DATABASE IF NOT EXISTS `lms` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
 USE `lms`;
 
--- --------------------------------------------------------
-
---
 -- Table structure for table `lms_admin`
 --
 
